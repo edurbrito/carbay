951bab9574a716cbba251fa0ef425a3e938f09d4
Eduardo Brito <up201806271@fe.up.pt>
2021-03-03 16:17:37 +0000

    add header and footer templates & fontawesome icons

--- html/index.php
+++ html/index.php
@@ -1,70 +1,14 @@
-<!DOCTYPE html>
-<html>
+<?php
+  include_once("templates/header.php");
+?>
+<img src="https://mrcollection.com/wp-content/uploads/2020/08/bugatti-chiron-pur-sport_01.jpg" class="img-fluid" alt="...">
 
-<head>
-  <meta charset="UTF-8">
-  <title>CarBay</title>
-  <link rel="stylesheet" href="./css/bootstrap.min.css">
-</head>
+<img src="https://mrcollection.com/wp-content/uploads/2020/08/bugatti-chiron-pur-sport_01.jpg" class="img-fluid" alt="...">
 
-<body class="bg-primary">
-<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+<img src="https://mrcollection.com/wp-content/uploads/2020/08/bugatti-chiron-pur-sport_01.jpg" class="img-fluid" alt="...">
 
-  <div class="collapse navbar-collapse" id="navbarColor01">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home
-          <span class="sr-only">(current)</span>
-        </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Features</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">About</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-        <div class="dropdown-menu">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <a class="dropdown-item" href="#">Something else here</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Separated link</a>
-        </div>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="text" placeholder="Search">
-      <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
+<img src="https://mrcollection.com/wp-content/uploads/2020/08/bugatti-chiron-pur-sport_01.jpg" class="img-fluid" alt="...">
 
-  <section class="container-lg">
-    <div class="jumbotron">
-      <h1 class="display-3">Hello, world!</h1>
-      <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-      <hr class="my-4">
-      <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-      <p class="lead">
-        <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-      </p>
-    </div>
-  </section>
-<nav class="navbar fixed-bottom navbar-dark bg-primary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Fixed bottom</a>
-  </div>
-</nav>
-</body>
-
-
-</html>
+<?php
+  include_once("templates/footer.php");
+?>
---
+++ html/templates/header.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link href="./css/bootstrap.min.css" rel="stylesheet">
+    <script src="./js/all.min.js" defer></script>
+    <title>CarBay</title>
+</head>
+
+<body class="bg-primary">
+    <nav class="navbar fixed-top navbar-expand-lg navbar-primary bg-primary text-light border-bottom border-danger" style="background-color:rgba(0, 0, 0, 0.8) !important;">
+        <div class="container-fluid">
+            <a class="navbar-brand text-light" href="#">
+                <i class="fas fa-car"></i>
+                CarBay
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon">
+                    <i class="fas fa-bars" style="color:#fff; font-size:28px;"></i>
+                </span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+                <ul class="navbar-nav d-flex justify-content-end align-items-center w-100">
+                    <li class="nav-item mr-0 mr-lg-4 navbar-brand ">
+                        <i class="fas fa-search"></i><span class="d-lg-none .d-xl-block"> Search</span>
+                    </li>
+                    <li class="nav-item navbar-brand mr-0 mr-lg-4">Log In</li>
+                    <li class="nav-item navbar-brand mr-0 mr-lg-4">Sign Up</li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+<div class="container-lg text-light overflow-auto my-5" style="margin-top: 8rem !important;">
