3a87e8e67aeaebc1df2f56390634663dbe4bd653
Paulo Ribeiro <pjsalgadomribeiro@gmail.com>
2021-03-05 20:20:48 +0000

    Done Users Ratings section on Profile page

--- html/profile/users-ratings.php
+++ html/profile/users-ratings.php
@@ -1,1 +1,136 @@
-Users Ratings Section
+<div class="container-fluid mx-auto">
+  <h1 class="w-100 text-light p-md-4 text-center">Users Ratings</h1>
+
+  <div class="panel-group" id="accordion">
+    <div class="panel panel-default">
+      <div class="card mt-5" style="background-color:#343434">
+        <div class="row no-gutters panel-heading">
+          <div class="col-auto">
+            <img src="https://www.schuetz-kleine.de/images/fmueller-rettstatt.jpg" class="img-fluid" alt="" style="min-height:50px;height:50px;min-width:50px;width:50px">
+          </div>
+          <div class="col-4 d-flex align-items-end">
+              <h4 class="card-title text-light ml-3">johndeer</h4>
+          </div>
+          <div class="col-6 d-flex align-items-center">
+            <h5 class="font-weight-bold text-light my-auto">Rated: <i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i></h5>
+          </div>
+          <div class="col-1 d-flex align-items-center justify-content-end">
+              <a data-bs-toggle="collapse" data-parent="#accordion" href="#collapse1" class="text-light"><i class="fas fa-chevron-down fa-3x"></i></a>
+          </div>
+        </div>
+      </div>
+      <div id="collapse1" class="panel-collapse collapse in">
+        <div class="panel-body text-light border border-top-0 rounded-bottom border-dark p-1" style="background-color:#5e5e5e">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+          commodo consequat.
+        </div>
+      </div>
+    </div>
+    <div class="panel panel-default">
+      <div class="card mt-3" style="background-color:#343434">
+        <div class="row no-gutters panel-heading">
+          <div class="col-auto">
+            <img src="https://media-exp1.licdn.com/dms/image/C5603AQGaHKtE2HKmrg/profile-displayphoto-shrink_200_200/0/1527672049782?e=1617840000&v=beta&t=BCH1OQxmS2idxk3HoirguIiiHs2Rv20RFIkxBx9cUv0" class="img-fluid" alt="" style="min-height:50px;height:50px;min-width:50px;width:50px">
+          </div>
+          <div class="col-4 d-flex align-items-end">
+              <h4 class="card-title text-light ml-3">rickwheels</h4>
+          </div>
+          <div class="col-6 d-flex align-items-center">
+            <h5 class="font-weight-bold text-light my-auto">Rated: <i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i></h5>
+          </div>
+          <div class="col-1 d-flex align-items-center justify-content-end">
+              <a data-bs-toggle="collapse" data-parent="#accordion" href="#collapse2" class="text-light"><i class="fas fa-chevron-down fa-3x"></i></a>
+          </div>
+        </div>
+      </div>
+      <div id="collapse2" class="panel-collapse collapse">
+        <div class="panel-body text-light border border-top-0 rounded-bottom border-dark p-1" style="background-color:#5e5e5e">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+          commodo consequat.
+        </div>
+      </div>
+    </div>
+    <div class="panel panel-default">
+      <div class="card mt-3" style="background-color:#343434">
+        <div class="row no-gutters panel-heading">
+          <div class="col-auto">
+            <img src="https://th.bing.com/th/id/OIP.j5E0wnPXgMi4MmSzeMxSGgHaJX?pid=ImgDet&w=1500&h=1897&rs=1" class="img-fluid" alt="" style="min-height:50px;height:50px;min-width:50px;width:50px">
+          </div>
+          <div class="col-4 d-flex align-items-end">
+              <h4 class="card-title text-light ml-3">sophyspeed</h4>
+          </div>
+          <div class="col-6 d-flex align-items-center">
+            <h5 class="font-weight-bold text-light my-auto">Rated: <i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i></h5>
+          </div>
+          <div class="col-1 d-flex align-items-center justify-content-end">
+              <a data-bs-toggle="collapse" data-parent="#accordion" href="#collapse3" class="text-light"><i class="fas fa-chevron-down fa-3x"></i></a>
+          </div>
+        </div>
+      </div>
+      <div id="collapse3" class="panel-collapse collapse">
+        <div class="panel-body text-light border border-top-0 rounded-bottom border-dark p-1" style="background-color:#5e5e5e">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+          commodo consequat.
+        </div>
+      </div>
+    </div>
+    <div class="panel panel-default">
+      <div class="card mt-3" style="background-color:#343434">
+        <div class="row no-gutters panel-heading">
+          <div class="col-auto">
+            <img src="https://th.bing.com/th/id/OIP.eboDRnxW1LHdpUmPpU2c8QAAAA?pid=ImgDet&w=151&h=181&rs=1" class="img-fluid" alt="" style="min-height:50px;height:50px;min-width:50px;width:50px">
+          </div>
+          <div class="col-4 d-flex align-items-end">
+              <h4 class="card-title text-light ml-3">mattsubufer</h4>
+          </div>
+          <div class="col-6 d-flex align-items-center">
+            <h5 class="font-weight-bold text-light my-auto">Rated: <i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i></h5>
+          </div>
+          <div class="col-1 d-flex align-items-center justify-content-end">
+              <a data-bs-toggle="collapse" data-parent="#accordion" href="#collapse4" class="text-light"><i class="fas fa-chevron-down fa-3x"></i></a>
+          </div>
+        </div>
+      </div>
+      <div id="collapse4" class="panel-collapse collapse in">
+        <div class="panel-body text-light border border-top-0 rounded-bottom border-dark p-1" style="background-color:#5e5e5e">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+          commodo consequat.
+        </div>
+      </div>
+    </div>
+    <div class="panel panel-default">
+      <div class="card mt-3" style="background-color:#343434">
+        <div class="row no-gutters panel-heading">
+          <div class="col-auto">
+            <img src="https://www.imro.ie/wp-content/plugins/widgetkit/cache/Saron-Boyse-IMRO-candidate-2019bw-92120e389b90b27dd329a5fe82ff57f6.jpg" class="img-fluid" alt="" style="min-height:50px;height:50px;min-width:50px;width:50px">
+          </div>
+          <div class="col-4 d-flex align-items-end">
+              <h4 class="card-title text-light ml-3">annadoors</h4>
+          </div>
+          <div class="col-6 d-flex align-items-center">
+            <h5 class="font-weight-bold text-light my-auto">Rated: <i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i></h5>
+          </div>
+          <div class="col-1 d-flex align-items-center justify-content-end">
+              <a data-bs-toggle="collapse" data-parent="#accordion" href="#collapse5" class="text-light"><i class="fas fa-chevron-down fa-3x"></i></a>
+          </div>
+        </div>
+      </div>
+      <div id="collapse5" class="panel-collapse collapse in">
+        <div class="panel-body text-light border border-top-0 rounded-bottom border-dark p-1" style="background-color:#5e5e5e">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+          commodo consequat.
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
