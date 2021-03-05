8e0bbd441546a0c47549aafd680fe2c1c20a423f
Paulo Ribeiro <pjsalgadomribeiro@gmail.com>
2021-03-05 18:17:28 +0000

    Done Auctions Created section on Profile page

--- html/profile/auctions-created.php
+++ html/profile/auctions-created.php
@@ -1,1 +1,40 @@
-Auctions Created Section
+<div class="container-fluid mx-auto">
+  <h1 class="w-100 text-light p-md-4 text-center">Auctions Created</h1>
+  <form class="row align-items-start position-relative">
+    <div class="col-11">
+      <label for="search" class="form-label">Search</label>
+      <input type="text" class="form-control" id="search" placeholder="Type Something">
+    </div>
+    <div class="col-1 align-self-end position-absolute top-50 end-0">
+      <i class="fas fa-search fa-2x"></i>
+    </div>
+  </form>
+</div>
+
+<div class="card mt-5" style="background-color:#343434">
+    <div class="row no-gutters">
+        <div class="col-auto">
+            <img src="https://mrcollection.com/wp-content/uploads/2018/07/ferrari-portofino-giallo_01.jpg" class="img-fluid" alt="" style="min-height:50px;height:50px;min-width:50px;width:50px">
+        </div>
+        <div class="col-8 align-self-end">
+            <h4 class="card-title text-light ml-3">Car Model D</h4>
+        </div>
+        <div class="col align-self-end">
+            <h4 class="card-title text-light">Highest Bid: 135€</h4>
+        </div>
+    </div>
+</div>
+
+<div class="card mt-3" style="background-color:#343434">
+    <div class="row no-gutters">
+        <div class="col-auto">
+            <img src="https://mrcollection.com/wp-content/uploads/2018/07/ferrari-portofino-giallo_01.jpg" class="img-fluid" alt="" style="min-height:50px;height:50px;min-width:50px;width:50px">
+        </div>
+        <div class="col-8 align-self-end">
+            <h4 class="card-title text-light ml-3">Car Model E</h4>
+        </div>
+        <div class="col align-self-end">
+            <h4 class="card-title text-light">Highest Bid: 62€</h4>
+        </div>
+    </div>
+</div>
