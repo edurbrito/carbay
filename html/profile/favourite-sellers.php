b3b04d0c58b0dcf899360d87b8fc9220839406cc
Paulo Ribeiro <pjsalgadomribeiro@gmail.com>
2021-03-05 18:42:08 +0000

    Done Favourite Sellers section on Profile page

--- html/profile/favourite-sellers.php
+++ html/profile/favourite-sellers.php
@@ -1,1 +1,40 @@
-Favourite Sellers Section
+<div class="container-fluid mx-auto">
+  <h1 class="w-100 text-light p-md-4 text-center">Favourite Sellers</h1>
+  <form class="row align-items-start position-relative">
+    <div class="col-11">
+      <label for="search" class="form-label">Search</label>
+      <input type="text" class="form-control" id="search" placeholder="Type Something">
+    </div>
+    <div class="col-1 align-self-end position-absolute top-50 end-0">
+      <i class="fas fa-search fa-2x"></i>
+    </div>
+  </form>
+</div>
+
+<div class="card mt-5" style="background-color:#343434">
+    <div class="row no-gutters">
+        <div class="col-auto">
+            <img src="https://media-exp1.licdn.com/dms/image/C5603AQGaHKtE2HKmrg/profile-displayphoto-shrink_200_200/0/1527672049782?e=1617840000&v=beta&t=BCH1OQxmS2idxk3HoirguIiiHs2Rv20RFIkxBx9cUv0" class="img-fluid" alt="" style="min-height:50px;height:50px;min-width:50px;width:50px">
+        </div>
+        <div class="col-11 align-self-end">
+            <h4 class="card-title text-light ml-3">rickwheels</h4>
+        </div>
+        <div class="col align-self-end">
+            <h4 class="card-title text-light"><i class="fas fa-times fa-lg"></i></h4>
+        </div>
+    </div>
+</div>
+
+<div class="card mt-3" style="background-color:#343434">
+    <div class="row no-gutters">
+        <div class="col-auto">
+            <img src="https://th.bing.com/th/id/OIP.j5E0wnPXgMi4MmSzeMxSGgHaJX?pid=ImgDet&w=1500&h=1897&rs=1" class="img-fluid" alt="" style="min-height:50px;height:50px;min-width:50px;width:50px">
+        </div>
+        <div class="col-11 align-self-end">
+            <h4 class="card-title text-light ml-3">sophyspeed</h4>
+        </div>
+        <div class="col align-self-end">
+            <h4 class="card-title text-light"><i class="fas fa-times fa-lg"></i></h4>
+        </div>
+    </div>
+</div>
