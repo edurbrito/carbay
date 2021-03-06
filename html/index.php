6c3ba0e26e7fc351490b8b726ba13af760f42539
Eduardo Brito <up201806271@fe.up.pt>
2021-03-06 22:12:19 +0000

    add homepage featured design

--- html/index.php
+++ html/index.php
@@ -1,196 +1,111 @@
 <?php
-include_once(__DIR__ . "/templates/header.php");
+include_once(__DIR__ . "/templates/head.php");
 ?>
 
-<div class="container">
-  <div class="row">
-    <div class="col-sm-12 col-md-12">
-      <div id="carouselExampleCaptions" class="carousel slide carousel-fade" data-bs-ride="carousel">
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img src="https://mrcollection.com/wp-content/uploads/2020/08/bugatti-chiron-pur-sport_01.jpg" class="img-fluid" alt="...">
-            <div class="carousel-caption d-none d-md-block">
-              <div class="float-xl-left">
-                <p class="fs-4" style="text-shadow: 2px 2px 0px #ff0000;">
-                  <i class="far fa-clock"></i>
-                  2d 7h 59m 22s
-                </p>
-                <p class="fs-4" style="text-shadow: 2px 2px 0px #ff0000;">
-                  <i class="fas fa-money-bill-wave-alt"></i>
-                  Last Bid: 500€
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="carousel-item">
-            <img src="https://mrcollection.com/wp-content/uploads/2020/11/lamborghini-aventador-svj-xago-edition.jpg" class="img-fluid" alt="...">
-            <div class="carousel-caption d-none d-md-block">
-              <div class="float-xl-left">
-                <p class="fs-4" style="text-shadow: 2px 2px 0px #ff0000;">
-                  <i class="far fa-clock"></i>
-                  2d 5h 47m 36s
-                </p>
-                <p class="fs-4" style="text-shadow: 2px 2px 0px #ff0000;">
-                  <i class="fas fa-money-bill-wave-alt"></i>
-                  Last Bid: 863€
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="carousel-item">
-            <img src="https://mrcollection.com/wp-content/uploads/2020/08/Lambo_V12_Vision_GT_02.jpg" class="img-fluid" alt="...">
-            <div class="carousel-caption d-none d-md-block">
-              <div class="float-xl-left">
-                <p class="fs-4" style="text-shadow: 2px 2px 0px #ff0000;">
-                  <i class="far fa-clock"></i>
-                  0d 2h 15m 47s
-                </p>
-                <p class="fs-4" style="text-shadow: 2px 2px 0px #ff0000;">
-                  <i class="fas fa-money-bill-wave-alt"></i>
-                  Last Bid: 1059€
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="carousel-item">
-            <img src="https://mrcollection.com/wp-content/uploads/2020/10/lamborghini-veneno_164.jpg" class="img-fluid" alt="...">
-            <div class="carousel-caption d-none d-md-block">
-              <div class="float-xl-left">
-                <p class="fs-4" style="text-shadow: 2px 2px 0px #ff0000;">
-                  <i class="far fa-clock"></i>
-                  4d 3h 34m 21s
-                </p>
-                <p class="fs-4" style="text-shadow: 2px 2px 0px #ff0000;">
-                  <i class="fas fa-money-bill-wave-alt"></i>
-                  Last Bid: 425€
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="carousel-item">
-            <img src="https://mrcollection.com/wp-content/uploads/2020/08/ferrari-812-gts_01.jpg" class="img-fluid" alt="...">
-            <div class="carousel-caption d-none d-md-block">
-              <div class="float-xl-left">
-                <p class="fs-4" style="text-shadow: 2px 2px 0px #ff0000;">
-                  <i class="far fa-clock"></i>
-                  1d 5h 20m 30s
-                </p>
-                <p class="fs-4" style="text-shadow: 2px 2px 0px #ff0000;">
-                  <i class="fas fa-money-bill-wave-alt"></i>
-                  Last Bid: 2019€
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="carousel-item">
-            <img src="https://mrcollection.com/wp-content/uploads/2020/09/bugatti-centodieci_01.jpg" class="img-fluid" alt="...">
-            <div class="carousel-caption d-none d-md-block">
-              <div class="float-xl-left">
-                <p class="fs-4" style="text-shadow: 2px 2px 0px #ff0000;">
-                  <i class="far fa-clock"></i>
-                  5d 2h 25m 38s
-                </p>
-                <p class="fs-4" style="text-shadow: 2px 2px 0px #ff0000;">
-                  <i class="fas fa-money-bill-wave-alt"></i>
-                  Last Bid: 780€
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="carousel-item">
-            <img src="https://mrcollection.com/wp-content/uploads/2020/09/ferrari-sf-90-stradale_01.jpg" class="img-fluid" alt="...">
-            <div class="carousel-caption d-none d-md-block">
-              <div class="float-xl-left">
-                <p class="fs-4" style="text-shadow: 2px 2px 0px #ff0000;">
-                  <i class="far fa-clock"></i>
-                  4d 12h 56m 04s
-                </p>
-                <p class="fs-4" style="text-shadow: 2px 2px 0px #ff0000;">
-                  <i class="fas fa-money-bill-wave-alt"></i>
-                  Last Bid: 1725€
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="carousel-item">
-            <img src="https://mrcollection.com/wp-content/uploads/2020/09/bugatti_la_voiture_noire_01.jpg" class="img-fluid" alt="...">
-            <div class="carousel-caption d-none d-md-block">
-              <div class="float-xl-left">
-                <p class="fs-4" style="text-shadow: 2px 2px 0px #ff0000;">
-                  <i class="far fa-clock"></i>
-                  2d 21h 17m 43s
-                </p>
-                <p class="fs-4" style="text-shadow: 2px 2px 0px #ff0000;">
-                  <i class="fas fa-money-bill-wave-alt"></i>
-                  Last Bid: 600€
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="carousel-item">
-            <img src="https://mrcollection.com/wp-content/uploads/2020/08/lamborghini_sian_fkp_37_02.jpg" class="img-fluid" alt="...">
-            <div class="carousel-caption d-none d-md-block">
-              <div class="float-xl-left">
-                <p class="fs-4" style="text-shadow: 2px 2px 0px #ff0000;">
-                  <i class="far fa-clock"></i>
-                  9d 1h 14m 52s
-                </p>
-                <p class="fs-4" style="text-shadow: 2px 2px 0px #ff0000;">
-                  <i class="fas fa-money-bill-wave-alt"></i>
-                  Last Bid: 260€
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="carousel-item">
-            <img src="https://mrcollection.com/wp-content/uploads/2020/08/Ferrari-Roma_03.jpg" class="img-fluid" alt="...">
-            <div class="carousel-caption d-none d-md-block">
-              <div class="float-xl-left">
-                <p class="fs-4" style="text-shadow: 2px 2px 0px #ff0000;">
-                  <i class="far fa-clock"></i>
-                  10d 17h 23m 36s
-                </p>
-                <p class="fs-4" style="text-shadow: 2px 2px 0px #ff0000;">
-                  <i class="fas fa-money-bill-wave-alt"></i>
-                  Last Bid: 400€
-                </p>
-              </div>
-            </div>
-          </div>
+<body class="bg-primary" style="height: 100vh; background-image: url(/css/cover.png); background-repeat: no-repeat; background-size: cover; background-position: center; background-image: linear-gradient(to bottom, rgba(0,0,0,0.45) 0%,rgba(0,0,0,0.45) 100%), url(/css/cover.png);">
+  <nav class="navbar navbar-expand-lg navbar-primary bg-primary text-light border-bottom border-dark" style="background-color:rgba(0, 0, 0, 0.8) !important;">
+    <div class="container-fluid">
+      <a class="navbar-brand text-light" href="/">
+        <img src="/css/icon.png" style="max-height: 36px;">
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon">
+          <i class="fas fa-bars" style="color:#fff; font-size:28px;"></i>
+        </span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+        <ul class="navbar-nav d-flex justify-content-end align-items-center w-100">
+          <li class="nav-item mr-0 mr-lg-4 navbar-brand ">
+            <a href="/search.php" class="text-decoration-none text-reset"><i class="fas fa-search"></i><span class="d-lg-none .d-xl-block"> Search</span></a>
+          </li>
+          <li class="nav-item navbar-brand mr-0 mr-lg-4">
+            <a class="text-light" href="/login.php">
+              Log In
+            </a>
+          </li>
+          <li class="nav-item navbar-brand mr-0 mr-lg-4">
+            <a class="text-light" href="/signup.php">
+              Sign Up
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 
-        </div>
-
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
-        </button>
-        <div class="carousel-indicators">
-          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
-          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="4" aria-label="Slide 5"></button>
-          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="5" aria-label="Slide 6"></button>
-          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="6" aria-label="Slide 7"></button>
-          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="7" aria-label="Slide 8"></button>
-          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="8" aria-label="Slide 9"></button>
-          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="9" aria-label="Slide 10"></button>
-        </div>
+  <div id="carousel" class="carousel slide" style="width: fit-content !important;" data-bs-ride="carousel" data-bs-interval="5000">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <a class="m-5 border-left p-3 d-flex flex-column text-decoration-none" href="/auction-page.php">
+          <h5 class="text-light">Ferrari 458 Italia</h5>
+          <div class="text-light">
+            <i class="far fa-clock"></i>
+            2d 7h 59m 22s
+          </div>
+          <div class="text-light">
+            <i class="fas fa-money-bill-wave-alt"></i>
+            Last Bid: 500€
+          </div>
+          <div class="text-light">
+            <i class="fas fa-wallet"></i>
+            Buy Now: 2000€
+          </div>
+        </a>
+      </div>
+      <div class="carousel-item">
+        <a class="m-5 border-left p-3 d-flex flex-column text-decoration-none" href="/auction-page.php">
+          <h5 class="text-light">Citroen do Paulo</h5>
+          <div class="text-light">
+            <i class="far fa-clock"></i>
+            2d 7h 59m 22s
+          </div>
+          <div class="text-light">
+            <i class="fas fa-money-bill-wave-alt"></i>
+            Last Bid: 500€
+          </div>
+          <div class="text-light">
+            <i class="fas fa-wallet"></i>
+            Buy Now: 2000€
+          </div>
+        </a>
+      </div>
+      <div class="carousel-item">
+        <a class="m-5 border-left p-3 d-flex flex-column text-decoration-none" href="/auction-page.php">
+          <h5 class="text-light">Lamborghini Tractor</h5>
+          <div class="text-light">
+            <i class="far fa-clock"></i>
+            2d 7h 59m 22s
+          </div>
+          <div class="text-light">
+            <i class="fas fa-money-bill-wave-alt"></i>
+            Last Bid: 500€
+          </div>
+          <div class="text-light">
+            <i class="fas fa-wallet"></i>
+            Buy Now: 2000€
+          </div>
+        </a>
       </div>
     </div>
   </div>
-  <div class="row mt-4">
-    <a class="d-grid gap-2 col-8 mx-auto" href="search.php">
-      <button class="btn btn-outline-danger" style="background-color:#FFF;" onMouseOut="this.style.background='#FFF'" onMouseOver="this.style.background='#d9544f'" type="button">View all live auctions</button>
-    </a>
+
+
+
+
+  <div class="fixed-bottom">
+    <div class="d-flex flex-column justify-content-center p-5" style="background: linear-gradient(transparent, black);">
+      <h1 class="text-light text-center mb-2" style="font-size: 2.5rem;">START BIDDING NOW</h1>
+      <h5 class="text-light text-center mb-3">We have all you need to complete your car collections</h5>
+      <a class="btn btn-dark mx-auto" style="width: fit-content;" href="/search.php">View All Auctions</a>
+    </div>
+    <nav class="navbar navbar-primary border-top border-dark" style="background-color: #000;">
+      <div class="container-fluid d-flex justify-content-center">
+        <a class="navbar-brand text-light" href="/about.php">About Us</a>
+        <a class="navbar-brand text-light" href="/faqs.php">FAQs</a>
+      </div>
+    </nav>
   </div>
-</div>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+</body>
 
-
-<?php
-include_once(__DIR__ . "/templates/footer.php");
-?>
+</html>
