f0bee5cc41b3f55f6148faae7b84c4673f7dc09c
Pedro Ferreira <up201806506@fe.up.pt>
2021-03-09 15:23:14 +0000

    added report comments

--- html/admin/reports.php
+++ html/admin/reports.php
@@ -3,83 +3,83 @@
         <div class="d-flex align-items-center justify-content-start rounded-0 flex-vertical">
             <div class="d-flex align-items-center justify-content-start mb-3 mb-sm-0">
                 <img src="https://images.theconversation.com/files/304864/original/file-20191203-67028-qfiw3k.jpeg?ixlib=rb-1.1.0&rect=638%2C2%2C795%2C745&q=45&auto=format&w=496&fit=clip" width="36px">
-                <span class="text-primary ml-3">John Doe</span>
+                <span class="text-primary ml-3">rickwheels</span>
             </div>
             <div class="btn-group ml-sm-auto" role="group" aria-label="User Management Buttons">
                 <button type="button" class="btn btn-danger mr-1">Ban</button>
                 <button type="button" class="btn btn-primary">Discard</button>
             </div>
         </div>
-        <p class="mt-3 text-primary text-justify"><strong class="text-primary">Reason:</strong> <br> Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus provident ad doloremque eius hic laboriosam ullam totam vitae aspernatur natus, autem aliquam? Mollitia perferendis similique nam quidem excepturi harum temporibus sit magnam nulla!</p>
+        <p class="mt-3 text-primary text-justify"><strong class="text-primary">Reason:</strong> <br> He threatened to come to my house and to steal all my car models. He is dangerous! </p>
     </li>
 
     <li class="list-group-item">
         <div class="d-flex align-items-center justify-content-start rounded-0 flex-vertical">
             <div class="d-flex align-items-center justify-content-start mb-3 mb-sm-0">
                 <img src="https://static.wikia.nocookie.net/mrbean/images/4/4b/Mr_beans_holiday_ver2.jpg" width="36px">
-                <span class="text-primary ml-3">Mary Rose</span>
+                <span class="text-primary ml-3">sophyspeed</span>
             </div>
             <div class="btn-group ml-sm-auto" role="group" aria-label="User Management Buttons">
                 <button type="button" class="btn btn-danger mr-1">Ban</button>
                 <button type="button" class="btn btn-primary">Discard</button>
             </div>
         </div>
-        <p class="mt-3 text-primary text-justify"><strong class="text-primary">Reason:</strong> <br> Lorem ipsum dolor sit.</p>
+        <p class="mt-3 text-primary text-justify"><strong class="text-primary">Reason:</strong> <br> Ban this person! He is always winning the auctions I want!</p>
     </li>
 
     <li class="list-group-item">
         <div class="d-flex align-items-center justify-content-start rounded-0 flex-vertical">
             <div class="d-flex align-items-center justify-content-start mb-3 mb-sm-0">
                 <img src="https://downloadwap.com/thumbs4/ringtones/covers/s/mrbean5.jpg" width="36px">
-                <span class="text-primary ml-3">Anthony Taylor</span>
+                <span class="text-primary ml-3">relampagomark</span>
             </div>
             <div class="btn-group ml-sm-auto" role="group" aria-label="User Management Buttons">
                 <button type="button" class="btn btn-danger mr-1">Ban</button>
                 <button type="button" class="btn btn-primary">Discard</button>
             </div>
         </div>
-        <p class="mt-3 text-primary text-justify"><strong class="text-primary">Reason:</strong> <br> Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem non iusto reiciendis sint? Laboriosam eveniet a modi molestias nostrum placeat minima hic accusantium, id inventore deserunt laborum animi explicabo.</p>
+        <p class="mt-3 text-primary text-justify"><strong class="text-primary">Reason:</strong> <br> This individual is selling car models he does not possess. Unacceptable!</p>
     </li>
 
     <li class="list-group-item">
         <div class="d-flex align-items-center justify-content-start rounded-0 flex-vertical">
             <div class="d-flex align-items-center justify-content-start mb-3 mb-sm-0">
                 <img src="https://images.theconversation.com/files/304864/original/file-20191203-67028-qfiw3k.jpeg?ixlib=rb-1.1.0&rect=638%2C2%2C795%2C745&q=45&auto=format&w=496&fit=clip" width="36px">
-                <span class="text-primary ml-3">John Doe</span>
+                <span class="text-primary ml-3">matethetruck</span>
             </div>
             <div class="btn-group ml-sm-auto" role="group" aria-label="User Management Buttons">
                 <button type="button" class="btn btn-danger mr-1">Ban</button>
                 <button type="button" class="btn btn-primary">Discard</button>
             </div>
         </div>
-        <p class="mt-3 text-primary text-justify"><strong class="text-primary">Reason:</strong> <br> Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+        <p class="mt-3 text-primary text-justify"><strong class="text-primary">Reason:</strong> <br> This user is selling fake models! All made in China with low-cost materials! ALL FAKE! Ban him, please! We cannot have people like this in our community! </p>
     </li>
 
     <li class="list-group-item">
         <div class="d-flex align-items-center justify-content-start rounded-0 flex-vertical">
             <div class="d-flex align-items-center justify-content-start mb-3 mb-sm-0">
                 <img src="https://static.wikia.nocookie.net/mrbean/images/4/4b/Mr_beans_holiday_ver2.jpg" width="36px">
-                <span class="text-primary ml-3">Mary Rose</span>
+                <span class="text-primary ml-3">herbiefarois</span>
             </div>
             <div class="btn-group ml-sm-auto" role="group" aria-label="User Management Buttons">
                 <button type="button" class="btn btn-danger mr-1">Ban</button>
                 <button type="button" class="btn btn-primary">Discard</button>
             </div>
         </div>
-        <p class="mt-3 text-primary text-justify"><strong class="text-primary">Reason:</strong> <br> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque, asperiores.</p>
+        <p class="mt-3 text-primary text-justify"><strong class="text-primary">Reason:</strong> <br> Not trustworthy! Ban asap.</p>
     </li>
 
     <li class="list-group-item">
         <div class="d-flex align-items-center justify-content-start rounded-0 flex-vertical">
             <div class="d-flex align-items-center justify-content-start mb-3 mb-sm-0">
                 <img src="https://downloadwap.com/thumbs4/ringtones/covers/s/mrbean5.jpg" width="36px">
-                <span class="text-primary ml-3">Anthony Taylor</span>
+                <span class="text-primary ml-3">dinocoazul</span>
             </div>
             <div class="btn-group ml-sm-auto" role="group" aria-label="User Management Buttons">
                 <button type="button" class="btn btn-danger mr-1">Ban</button>
                 <button type="button" class="btn btn-primary">Discard</button>
             </div>
         </div>
-        <p class="mt-3 text-primary text-justify"><strong class="text-primary">Reason:</strong> <br> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero suscipit ducimus nobis ea unde?</p>
+        <p class="mt-3 text-primary text-justify"><strong class="text-primary">Reason:</strong> <br> He sent me a message threatening my family!! This is completely unacceptable. All because I said I was not interested in the product and ended up buying it. Business skills are not allowed now? LOL. Ban him!</p>
     </li>
 </ol>
