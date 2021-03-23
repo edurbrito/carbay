a7c48c2f78d2c4265f4838c0b34847b1191c8f4d
Pedro <up201809694@fe.up.pt>
2021-03-23 17:00:51 +0000

    Database functional

--- database.sql
+++ database.sql
@@ -1,35 +1,97 @@
 DROP TABLE IF EXISTS "User";
 
 CREATE TABLE "User" (
-    id                  INTEGER,
-    name                VARCHAR(50),
-    username            VARCHAR(50),
-    email               VARCHAR(50),
-    password            VARCHAR(50),
-    image               VARCHAR(50),
-    banned              BOOLEAN DEFAULT FALSE,
-    admin               BOOLEAN DEFAULT FALSE       
+    id                  SERIAL,
+    name                VARCHAR(50) NOT NULL,
+    username            VARCHAR(50) NOT NULL,
+    email               VARCHAR(50) NOT NULL,
+    password            VARCHAR(50) NOT NULL,
+    image               VARCHAR(50) NOT NULL,
+    banned              BOOLEAN DEFAULT FALSE NOT NULL,
+    admin               BOOLEAN DEFAULT FALSE NOT NULL,
 
-    CONSTRAINT UserPK SERIAL PRIMARY KEY (id),
-    CONSTRAINT UserNameNN NOT NULL (name),
-    CONSTRAINT UserUsernameNN NOT NULL (username),
+    CONSTRAINT UserPK PRIMARY KEY (id),
     CONSTRAINT UserUsernameUK UNIQUE (username),
-    CONSTRAINT UserEmailNN NOT NULL (email),
-    CONSTRAINT UserEmailUK UNIQUE (email),
-    CONSTRAINT UserPasswordNN NOT NULL (password),
-    CONSTRAINT UserImageNN NOT NULL (image),
-    CONSTRAINT UserBannedNN NOT NULL (banned),
-    CONSTRAINT UserAdminNN NOT NULL (admin)
+    CONSTRAINT UserEmailUK UNIQUE (email)
+);
+
+CREATE TYPE Scale as ENUM (
+  '1:8',
+  '1:18',
+  '1:43',
+  '1:64'
+);
+
+DROP TABLE IF EXISTS Colour;
+
+CREATE TABLE Colour (
+    id                      SERIAL,
+    name                    VARCHAR(50) NOT NULL,
+
+    CONSTRAINT ColourPK PRIMARY KEY (id),
+    CONSTRAINT ColourNameUK UNIQUE (name)
+
+);
+
+
+DROP TABLE IF EXISTS Brand;
+
+CREATE TABLE Brand (
+    id                      SERIAL,
+    name                    VARCHAR(50) NOT NULL,
+
+    CONSTRAINT BrandPK PRIMARY KEY (id),
+    CONSTRAINT BrandNameUK UNIQUE (name)
+
+);
+
+
+DROP TABLE IF EXISTS Auction;
+
+CREATE TABLE Auction (
+    id                       SERIAL,
+    title                    VARCHAR(50) NOT NULL,
+    startingPrice            DECIMAL NOT NULL,
+    startDate                TIMESTAMP WITH TIME zone DEFAULT now() NOT NULL,
+    finalDate                TIMESTAMP WITH TIME zone NOT NULL,
+    suspend                  BOOLEAN DEFAULT FALSE NOT NULL,
+    buyNow                   DECIMAL,
+    scaleType                Scale NOT NULL,
+    brandID                  INTEGER NOT NULL,
+    colourID                 INTEGER NOT NULL,
+    sellerID                 INTEGER NOT NULL,
+
+    CONSTRAINT AuctionPK PRIMARY KEY (id),
+    CONSTRAINT AuctionStartingPriceCK CHECK(startingPrice >= 1),
+    CONSTRAINT AuctionStartDateCK CHECK(startDate >= now()),
+    CONSTRAINT AuctionFinalDateCK CHECK(finalDate >= startDate),
+    CONSTRAINT AuctionBuyNowCK CHECK (buyNow IS NULL or (buyNow IS NOT NULL and buyNow > startingPrice)),
+    CONSTRAINT AuctionBrandFK FOREIGN KEY (brandID) REFERENCES Brand ON UPDATE CASCADE ON DELETE RESTRICT,
+    CONSTRAINT AuctionColourFK FOREIGN KEY (colourID) REFERENCES Colour ON UPDATE CASCADE ON DELETE RESTRICT,
+    CONSTRAINT AuctionSellerFK FOREIGN KEY (sellerID) REFERENCES "User" ON UPDATE CASCADE ON DELETE RESTRICT
+);
+
+
+DROP TABLE IF EXISTS Image;
+
+CREATE TABLE Image (
+    id                      SERIAL,
+    url                     VARCHAR(50) NOT NULL,
+    auctionID               INTEGER NOT NULL,
+
+    CONSTRAINT ImagePK PRIMARY KEY (id),
+    CONSTRAINT ImageUrlUK UNIQUE (url),
+    CONSTRAINT ImageAuctionFK FOREIGN KEY (auctionID) REFERENCES Auction ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 
 DROP TABLE IF EXISTS FavouriteSeller;
 
 CREATE TABLE FavouriteSeller (
-    user1ID                 INTEGER,
-    user2ID                 INTEGER,
+    user1ID                 SERIAL,
+    user2ID                 SERIAL,
 
-    CONSTRAINT FavouriteSellerPK SERIAL PRIMARY KEY (user1ID, user2ID),
+    CONSTRAINT FavouriteSellerPK PRIMARY KEY (user1ID, user2ID),
     CONSTRAINT FavouriteSellerUser1FK FOREIGN KEY (user1ID) REFERENCES "User" ON UPDATE CASCADE ON DELETE CASCADE,
     CONSTRAINT FavouriteSellerUser2FK FOREIGN KEY (user2ID) REFERENCES "User" ON UPDATE CASCADE ON DELETE CASCADE
 );
@@ -38,10 +100,10 @@ CREATE TABLE FavouriteSeller (
 DROP TABLE IF EXISTS FavouriteAuction;
 
 CREATE TABLE FavouriteAuction (
-    userID                 INTEGER,
-    auctionID              INTEGER,
+    userID                 SERIAL,
+    auctionID              SERIAL,
 
-    CONSTRAINT FavouriteAuctionPK SERIAL PRIMARY KEY (userID,auctionID),
+    CONSTRAINT FavouriteAuctionPK PRIMARY KEY (userID,auctionID),
     CONSTRAINT FavouriteAuctionUserFK FOREIGN KEY (userID) REFERENCES "User" ON UPDATE CASCADE ON DELETE CASCADE,
     CONSTRAINT FavouriteAutionAuctionFK FOREIGN KEY (auctionID) REFERENCES Auction ON UPDATE CASCADE ON DELETE CASCADE
 );
@@ -50,21 +112,16 @@ CREATE TABLE FavouriteAuction (
 DROP TABLE IF EXISTS HelpMessage;
 
 CREATE TABLE HelpMessage (
-    id                      INTEGER,
-    text                    VARCHAR(50),
-    dateHour                DATETIME DEFAULT now(),
-    read                    BOOLEAN DEFAULT FALSE,
-    senderID                INTEGER,
-    recipientID             INTEGER,
+    id                      SERIAL,
+    text                    VARCHAR(50) NOT NULL,
+    dateHour                TIMESTAMP WITH TIME zone DEFAULT now() NOT NULL,
+    read                    BOOLEAN DEFAULT FALSE NOT NULL,
+    senderID                INTEGER NOT NULL,
+    recipientID             INTEGER NOT NULL,
     
-    CONSTRAINT HelpMessagePK SERIAL PRIMARY KEY (id),
-    CONSTRAINT HelpMessageTextNN NOT NULL (text),
-    CONSTRAINT HelpMessageDateNN NOT NULL (dateHour),
+    CONSTRAINT HelpMessagePK PRIMARY KEY (id),
     CONSTRAINT HelpMessageDateLT CHECK (dateHour <= now()),
-    CONSTRAINT HelpMessageReadNN NOT NULL (read),
-    CONSTRAINT HelpMessageSenderNN NOT NULL (senderID),
     CONSTRAINT HelpMessageSenderFK FOREIGN KEY (senderID) REFERENCES "User" ON UPDATE CASCADE ON DELETE SET NULL,
-    CONSTRAINT HelpMessageRecipientNN NOT NULL (recipientID),
     CONSTRAINT HelpMessageRecipientFK FOREIGN KEY (recipientID) REFERENCES "User" ON UPDATE CASCADE ON DELETE SET NULL
 );
 
@@ -72,41 +129,54 @@ CREATE TABLE HelpMessage (
 DROP TABLE IF EXISTS Rating;
 
 CREATE TABLE Rating (
-    auctionID               INTEGER,
-    winnerID                INTEGER,
-    value                   INTEGER,
-    dateHour                DATETIME DEFAULT now(),
-    comment                 VARCHAR(50),
+    auctionID               SERIAL,
+    winnerID                INTEGER NOT NULL,
+    value                   INTEGER NOT NULL,
+    dateHour                TIMESTAMP WITH TIME zone DEFAULT now() NOT NULL,
+    comment                 VARCHAR(50) NOT NULL,
 
-    CONSTRAINT RatingPK SERIAL PRIMARY KEY (auctionID),
+    CONSTRAINT RatingPK PRIMARY KEY (auctionID),
     CONSTRAINT RatingAuctionFK FOREIGN KEY (auctionID) REFERENCES Auction ON UPDATE CASCADE ON DELETE CASCADE,
-    CONSTRAINT RatingWinnerNN NOT NULL (winnerID),
     CONSTRAINT RatingWinnerFK FOREIGN KEY (winnerID) REFERENCES "User" ON UPDATE CASCADE ON DELETE SET NULL,
-    CONSTRAINT RatingValueNN NOT NULL (value),
     CONSTRAINT RatingValueHT CHECK (value >= 1),
     CONSTRAINT RatingValueLT CHECK (value <= 5),
-    CONSTRAINT RatingDateNN NOT NULL (dateHour),
-    CONSTRAINT RatingDateLT CHECK (dateHour <= now()),
-    CONSTRAINT RatingCommentNN NOT NULL (comment)
+    CONSTRAINT RatingDateLT CHECK (dateHour <= now())
 );
 
+CREATE TYPE State as ENUM (
+  'Waiting',
+  'Discarded',
+  'Banned'
+);
+
+DROP TABLE IF EXISTS Comment;
+
+CREATE TABLE Comment (
+    id                      SERIAL,
+    text                    VARCHAR(50) NOT NULL,
+    dateHour               TIMESTAMP WITH TIME zone DEFAULT now() NOT NULL,
+    authorID                INTEGER NOT NULL,
+    auctionID               INTEGER NOT NULL,
+    
+    CONSTRAINT CommentPK PRIMARY KEY (id),
+    CONSTRAINT CommentDateLT CHECK (dateHour <= now()),
+    CONSTRAINT CommentAuthorFK FOREIGN KEY (authorID) REFERENCES "User" ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT CommentAuctionFK FOREIGN KEY (auctionID) REFERENCES Auction ON UPDATE CASCADE ON DELETE CASCADE
+);
 
 DROP TABLE IF EXISTS Report;
 
 CREATE TABLE Report(
-    id                        INTEGER,
-    reason                    VARCHAR(50),
-    dateHour                  DATETIME DEFAULT now(),
-    reporterID                INTEGER,
+    id                        SERIAL,
+    reason                    VARCHAR(50) NOT NULL,
+    dateHour                  TIMESTAMP WITH TIME zone DEFAULT now() NOT NULL,
+    reporterID                INTEGER NOT NULL,
     locationAuctionID         INTEGER,
     locationCommentID         INTEGER,
     locationRegisteredID      INTEGER,
-    stateType                 State DEFAULT "Waiting",
+    stateType                 State DEFAULT 'Waiting' NOT NULL,
 
-    CONSTRAINT ReportPK SERIAL PRIMARY KEY (id),
-    CONSTRAINT ReportReasonNN NOT NULL (reason),
-    CONSTRAINT ReportDateNN NOT NULL (dateHour),
-    CONSTRAINT ReportReporterNN NOT NULL (reporterID),
+    CONSTRAINT ReportPK PRIMARY KEY (id),
     CONSTRAINT ReportReporterFK FOREIGN KEY (reporterID) REFERENCES "User" ON UPDATE CASCADE ON DELETE SET NULL,
     CONSTRAINT ReportExclusiveORLocation CHECK (
         1 = (
@@ -117,150 +187,34 @@ CREATE TABLE Report(
     ),
     CONSTRAINT ReportLocationAuctionFK FOREIGN KEY (locationAuctionID) REFERENCES Auction ON UPDATE CASCADE ON DELETE CASCADE,
     CONSTRAINT ReportLocationCommentFK FOREIGN KEY (locationCommentID) REFERENCES Comment ON UPDATE CASCADE ON DELETE CASCADE,
-    CONSTRAINT ReportLocationRegisteredFK FOREIGN KEY (locationRegisteredID) REFERENCES "User" ON UPDATE CASCADE ON DELETE CASCADE,
-    CONSTRAINT ReportStateTypeNN NOT NULL (stateType)
-);
-
-
-CREATE TYPE State as ENUM (
-  "Waiting",
-  "Discarded",
-  "Banned"
-);
-
-
-DROP TABLE IF EXISTS Auction;
-
-CREATE TABLE Auction (
-    id                       INTEGER,
-    title                    VARCHAR(50),
-    startingPrice            DECIMAL,
-    startDate                DATETIME DEFAULT now(),
-    finalDate                DATETIME,
-    suspend                  BOOLEAN DEFAULT FALSE,
-    buyNow                   DECIMAL,
-    scaleType                Scale,
-    brandID                  INTEGER,
-    colourID                 INTEGER,
-    sellerID                 INTEGER,
-
-    CONSTRAINT AuctionPK SERIAL PRIMARY KEY (id),
-    CONSTRAINT AuctionTitleNN NOT NULL(title),
-    CONSTRAINT AuctionStartingPriceNN NOT NULL(startingPrice),
-    CONSTRAINT AuctionStartingPriceCK CHECK(startingPrice >= 1),
-    CONSTRAINT AuctionStartDateNN NOT NULL(startDate),
-    CONSTRAINT AuctionStartDateCK CHECK(startDate >= now()),
-    CONSTRAINT AuctionFinalDateNN NOT NULL(finalDate),
-    CONSTRAINT AuctionStartDateCK CHECK(finalDate >= startDate),
-    CONSTRAINT AuctionSuspendNN NOT NULL(suspend),
-    CONSTRAINT AuctionBuyNowCK CHECK (buyNow IS NULL or (buyNow IS NOT NULL and buyNow > startingPrice)),
-    CONSTRAINT AuctionScaleTypeNN NOT NULL(scaleType),
-    CONSTRAINT AuctionBrandNN NOT NULL (brandID),
-    CONSTRAINT AuctionBrandFK FOREIGN KEY (brandID) REFERENCES Brand ON UPDATE CASCADE ON DELETE RESTRICT,
-    CONSTRAINT AuctionColourNN NOT NULL (colourID),
-    CONSTRAINT AuctionColourFK FOREIGN KEY (colourID) REFERENCES Colour ON UPDATE CASCADE ON DELETE RESTRICT,
-    CONSTRAINT AuctionSellerNN NOT NULL (sellerID),
-    CONSTRAINT AuctionSellerFK FOREIGN KEY (sellerID) REFERENCES "User" ON UPDATE CASCADE ON DELETE RESTRICT
-);
-
-
-CREATE TYPE Scale as ENUM (
-  "1:8",
-  "1:18",
-  "1:43",
-  "1:64"
-);
-
-
-DROP TABLE IF EXISTS Image;
-
-CREATE TABLE Image (
-    id                      INTEGER,
-    url                     VARCHAR(50),
-    auctionID               INTEGER,
-
-    CONSTRAINT ImagePK SERIAL PRIMARY KEY (id),
-    CONSTRAINT ImageUrlNN NOT NULL (url),
-    CONSTRAINT ImageUrlUK UNIQUE (url),
-    CONSTRAINT ImageAuctionNN NOT NULL (auctionID),
-    CONSTRAINT ImageAuctionFK FOREIGN KEY (auctionID) REFERENCES (Auction) ON UPDATE CASCADE ON DELETE CASCADE
-);
-
-
-DROP TABLE IF EXISTS Colour;
-
-CREATE TABLE Colour (
-    id                      INTEGER,
-    name                    VARCHAR(50),
-
-    CONSTRAINT ColourPK SERIAL PRIMARY KEY (id),
-    CONSTRAINT ColourNameNN NOT NULL (name),
-    CONSTRAINT ColourNameUK UNIQUE (name)
-
-);
-
-
-DROP TABLE IF EXISTS Brand;
-
-CREATE TABLE Brand (
-    id                      INTEGER,
-    name                    VARCHAR(50),
-
-    CONSTRAINT BrandPK SERIAL PRIMARY KEY (id),
-    CONSTRAINT BrandNameNN NOT NULL (name),
-    CONSTRAINT BrandNameUK UNIQUE (name)
-
+    CONSTRAINT ReportLocationRegisteredFK FOREIGN KEY (locationRegisteredID) REFERENCES "User" ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 
 DROP TABLE IF EXISTS Bid;
 
 CREATE TABLE Bid (
-    id                      INTEGER,
-    value                   DECIMAL,
-    dateHour                DATETIME DEFAULT now(),
-    authorID                INTEGER,
-    auctionID               INTEGER,
+    id                      SERIAL,
+    value                   DECIMAL NOT NULL,
+    dateHour                TIMESTAMP WITH TIME zone DEFAULT now() NOT NULL,
+    authorID                INTEGER NOT NULL,
+    auctionID               INTEGER NOT NULL,
 
-    CONSTRAINT BidPK SERIAL PRIMARY KEY (id),
-    CONSTRAINT BidValueNN NOT NULL (value),
-    CONSTRAINT BidDateNN NOT NULL (dateHour),
+    CONSTRAINT BidPK PRIMARY KEY (id),
     CONSTRAINT BidDateCK CHECK(dateHour <= now()),
-    CONSTRAINT BidAuthorNN NOT NULL (authorID),
     CONSTRAINT BidAuthorFK FOREIGN KEY (authorID) REFERENCES "User" ON UPDATE CASCADE ON DELETE RESTRICT,
-    CONSTRAINT BidAuctionNN NOT NULL (auctionID),
     CONSTRAINT BidAuctionFK FOREIGN KEY (auctionID) REFERENCES Auction ON UPDATE CASCADE ON DELETE RESTRICT
-);
-
-
-DROP TABLE IF EXISTS Comment;
-
-CREATE TABLE Comment (
-    id                      INTEGER,
-    text                    VARCHAR(50),
-    dateHour                DATETIME DEFAULT now(),
-    authorID                INTEGER,
-    auctionID               INTEGER,
-    
-    CONSTRAINT CommentPK SERIAL PRIMARY KEY (id),
-    CONSTRAINT CommentTextNN NOT NULL (text),
-    CONSTRAINT CommentDateNN NOT NULL (dateHour),
-    CONSTRAINT CommentDateLT CHECK (dateHour <= now()),
-    CONSTRAINT CommentAuthorNN NOT NULL (authorID),
-    CONSTRAINT CommentAuthorFK FOREIGN KEY (authorID) REFERENCES "User" ON UPDATE CASCADE ON DELETE CASCADE,
-    CONSTRAINT CommentAuctionNN NOT NULL (auctionID),
-    CONSTRAINT CommentAuctionFK FOREIGN KEY (auctionID) REFERENCES Auction ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 
 DROP TABLE IF EXISTS Notification;
 
 CREATE TABLE Notification (
-    id                      INTEGER,
-    text                    VARCHAR(50),
-    viewed                  BOOLEAN DEFAULT FALSE,
-    dateHour                DATETIME DEFAULT now(),
-    recipientID             INTEGER,
+    id                      SERIAL,
+    text                    VARCHAR(50) NOT NULL,
+    viewed                  BOOLEAN DEFAULT FALSE NOT NULL,
+    dateHour                TIMESTAMP WITH TIME zone DEFAULT now() NOT NULL,
+    recipientID             INTEGER NOT NULL,
     contextAuctionID        INTEGER,
     contextRatingID         INTEGER,
     contextHelpMessageID    INTEGER,
@@ -268,12 +222,8 @@ CREATE TABLE Notification (
     contextBidID            INTEGER,
     contextCommentID        INTEGER,
 
-    CONSTRAINT NotificationPK SERIAL PRIMARY KEY (id),
-    CONSTRAINT NotificationTextNN NOT NULL (text),
-    CONSTRAINT NotificationViewedNN NOT NULL (viewed),
-    CONSTRAINT NotificationDateNN NOT NULL (dateHour),
+    CONSTRAINT NotificationPK PRIMARY KEY (id),
     CONSTRAINT NotificationDateLT CHECK (dateHour <= now()),
-    CONSTRAINT NotificationRecipientNN NOT NULL (recipientID),
     CONSTRAINT NotificationExclusiveORContext CHECK (
         1 = (
             CASE WHEN contextAuctionID IS NOT NULL THEN 1 ELSE 0 END +
