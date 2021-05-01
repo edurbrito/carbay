3095a2b7957965fefa50961201a84010d1cd627c
Eduardo Brito <up201806271@fe.up.pt>
2021-05-01 19:20:35 +0100

    remove any reference to cards & items

--- routes/web.php
+++ routes/web.php
@@ -17,17 +17,6 @@ Route::get('/about', 'StaticController@about');
 // FAQs
 Route::get('/faqs', 'StaticController@faqs');
 
-// Cards
-Route::get('cards', 'CardController@list');
-Route::get('cards/{id}', 'CardController@show');
-
-// API
-Route::put('api/cards', 'CardController@create');
-Route::delete('api/cards/{card_id}', 'CardController@delete');
-Route::put('api/cards/{card_id}/', 'ItemController@create');
-Route::post('api/item/{id}', 'ItemController@update');
-Route::delete('api/item/{id}', 'ItemController@delete');
-
 // Authentication
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login');
