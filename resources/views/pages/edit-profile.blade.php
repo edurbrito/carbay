89a4d35d54890296c6a356d5f6148a39e70b2002
Paulo Ribeiro <pjsalgadomribeiro@gmail.com>
2021-05-09 11:21:10 +0100

    Fixed errors positions

--- resources/views/pages/edit-profile.blade.php
+++ resources/views/pages/edit-profile.blade.php
@@ -42,57 +42,57 @@
                   <span class="input-group-text rounded-0"><i class="fas fa-id-card"></i></span>
                 </div>
                 <input type="text" name="name" class="form-control" value="{{ old('name') != null ? old('name') : $user->name }}" placeholder="Name" required>
-                @if ($errors->has('name'))
-                <div onclick="this.hidden = true" class="alert alert-danger alert-dismissible fade show mb-3 p-1 px-2" role="alert">
-                  {{ $errors->first('name') }}
-                </div>
-                @endif
               </div>
+              @if ($errors->has('name'))
+              <div onclick="this.hidden = true" class="alert alert-danger alert-dismissible fade show mb-3 p-1 px-2" role="alert">
+                {{ $errors->first('name') }}
+              </div>
+              @endif
               <div class="input-group mb-3">
                 <div class="input-group-append">
                   <span class="input-group-text rounded-0"><i class="fas fa-at"></i></span>
                 </div>
                 <input type="email" name="email" class="form-control" value="{{ old('email') != null ? old('email') : $user->email }}" placeholder="Email" required>
-                @if ($errors->has('email'))
-                <div onclick="this.hidden = true" class="alert alert-danger alert-dismissible fade show mb-3 p-1 px-2" role="alert">
-                  {{ $errors->first('email') }}
-                </div>
-                @endif
               </div>
+              @if ($errors->has('email'))
+              <div onclick="this.hidden = true" class="alert alert-danger alert-dismissible fade show mb-3 p-1 px-2" role="alert">
+                {{ $errors->first('email') }}
+              </div>
+              @endif
               <div class="input-group mb-3">
                 <div class="input-group-append">
                   <span class="input-group-text rounded-0"><i class="fas fa-key"></i></span>
                 </div>
                 <input type="password" name="current_password" class="form-control" value="" placeholder="Current Password" required>
-                @if ($errors->has('current_password') || $errors->has('match'))
-                <div onclick="this.hidden = true" class="alert alert-danger alert-dismissible fade show mb-3 p-1 px-2" role="alert">
-                  {{ $errors->first('current_password') }}
-                  {{ $errors->first('match') }}
-                </div>
-                @endif
               </div>
+              @if ($errors->has('current_password') || $errors->has('match'))
+              <div onclick="this.hidden = true" class="alert alert-danger alert-dismissible fade show mb-3 p-1 px-2" role="alert">
+                {{ $errors->first('current_password') }}
+                {{ $errors->first('match') }}
+              </div>
+              @endif
               <div class="input-group mb-3">
                 <div class="input-group-append">
                   <span class="input-group-text rounded-0"><i class="fas fa-key"></i></span>
                 </div>
                 <input type="password" name="new_password" class="form-control" value="" placeholder="New Password">
-                @if ($errors->has('new_password'))
-                <div onclick="this.hidden = true" class="alert alert-danger alert-dismissible fade show mb-3 p-1 px-2" role="alert">
-                  {{ $errors->first('new_password') }}
-                </div>
-                @endif
               </div>
+              @if ($errors->has('new_password'))
+              <div onclick="this.hidden = true" class="alert alert-danger alert-dismissible fade show mb-3 p-1 px-2" role="alert">
+                {{ $errors->first('new_password') }}
+              </div>
+              @endif
               <div class="input-group mb-2">
                 <div class="input-group-append">
                   <span class="input-group-text rounded-0"><i class="fas fa-key"></i></span>
                 </div>
                 <input type="password" name="confirm_password" class="form-control" value="" placeholder="Repeat New Password">
-                @if ($errors->has('confirm_password'))
-                <div onclick="this.hidden = true" class="alert alert-danger alert-dismissible fade show mb-3 p-1 px-2" role="alert">
-                  {{ $errors->first('confirm_password') }}
-                </div>
-                @endif
               </div>
+              @if ($errors->has('confirm_password'))
+              <div onclick="this.hidden = true" class="alert alert-danger alert-dismissible fade show mb-3 p-1 px-2" role="alert">
+                {{ $errors->first('confirm_password') }}
+              </div>
+              @endif
               <div class="d-flex justify-content-center mt-3">
                 <button class="btn btn-dark text-center mr-3" type="submit" name="button" class="btn">Discard Changes</button>
                 <button class="btn btn-success text-light text-center " type="submit" name="button" class="btn">Save Changes</button>
